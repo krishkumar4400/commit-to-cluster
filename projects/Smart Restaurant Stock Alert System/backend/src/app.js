80e632ed0796ee76803909546f5c8db3436e9c8d
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-08-01 23:28:03 +0530

    Add backend and frontend setup with Docker and package management
    - Created package.json for backend with necessary dependencies and scripts.
    - Added Dockerfile for frontend to set up Node.js environment.
    - Introduced docker-compose.yml to orchestrate backend and frontend services.

--- projects/Smart Restaurant Stock Alert System/backend/src/app.js
+++ projects/Smart Restaurant Stock Alert System/backend/src/app.js
@@ -1,1 +1,23 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+const app = express();
+
+// middlewares
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+// routes
+
+
+export default app;
