e1d7c55723e8cba809d588dd5f6db545123ec274
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-08-02 23:38:06 +0530

    feat: add Spring Boot application with Docker support and Maven wrapper

---
+++ 08-docker/SpringBootApplication/src/main/java/com/krish/RestDemo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.krish.RestDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello World";
+    }
+}
