131180756fb48559f8c380725f2f470ba6acab8a
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-08-18 00:15:00 +0530

    Add backend service with Express and Docker support
    Created package.json for backend service with Express and Morgan dependencies.
    
    Implemented server.js to set up a basic Express server.
    
    Added Kubernetes deployment configuration for the Express application.
    
    Created Dockerfile for Smart Restaurant Stock Alert System to containerize the application.
    
    Added initial auth.controller.js file for future authentication logic.
    
    Set up docker-compose.yml for managing multi-container Docker applications.

--- 10-kubernetes/02-kubernetes/backend/products/server.js
+++ 10-kubernetes/02-kubernetes/backend/products/server.js
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 
 app.get("/api/product", (req, res) => {
-  const response = await axios.get('http://main-server-service');
+  const response = await axios.get('http://main-server-service/');
   return res.status(200).json({
     message: "success",
     success: true,
