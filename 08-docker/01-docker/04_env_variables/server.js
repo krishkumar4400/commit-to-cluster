fce8b5084f74da7b5777fa021ca567e8d8676d83
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-09-11 18:36:36 +0530

    Add environment variables to docker container using command line

---
+++ 08-docker/01-docker/04_env_variables/server.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  // res.send('Hello Express');
+  return res.status(200).json({
+    message: "Hello Express",
+    success: true,
+    environment: process.env.NODE_ENV || "DEFAULT",
+  });
+});
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
+
+/**
+ * docker build -t node-app .
+ * docker run --rm --name=node-app  -e PORT=4040 -p 4040:4040  node-app
+ * docker run --rm --name=node-app -p 4040:4040 -e PORT=4040 -e  NODE_ENV="development" node-app:v2
+ *
+ */
