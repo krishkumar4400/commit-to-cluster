b8d51add69b38125fe7da68e5cfe69bffe168dc1
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-09-11 22:12:22 +0530

    Add Docker setup with Dockerfile, docker-compose, and server implementation

--- 08-docker/01-docker/05-docker-compose/app-1/server.js
+++ 08-docker/01-docker/05-docker-compose/app-1/server.js
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -17,3 +20,16 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
 });
+
+/**
+ * docker compose up
+ * docker compose up -d
+ * docker compose down
+ * docker compose ps
+ * docker compose logs
+ * docker compose logs node-app
+ * docker compose logs -f node-app -> watch logs live of backend service
+ * docker compose logs -f  -> watch logs live of all services
+ * docker compose exec backend sh -> access shell of service 
+ * docker compose build
+ */
