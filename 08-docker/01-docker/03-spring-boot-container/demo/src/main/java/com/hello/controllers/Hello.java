cd7081a5e1ae0426fa209e2822f61178b8f9f9c7
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-09-11 16:53:59 +0530

    Add Dockerfile and Hello class for Spring Boot container setup

--- 08-docker/01-docker/03-spring-boot-container/demo/src/main/java/com/hello/controllers/Hello.java
+++ 08-docker/01-docker/03-spring-boot-container/demo/src/main/java/com/hello/controllers/Hello.java
@@ -1,4 +1,14 @@
 package com.hello.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class Hello {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
